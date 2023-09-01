fc86f1035404c3ba2637a245db5db408f6e5c1ae
Sadburritos <134071785+Sadburritos@users.noreply.github.com>
2023-09-01 21:15:34 +0600

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,31 +1,35 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-
+from PyQt5.QtGui import *
 
 class My_calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Calculator')
-        self.setGeometry(100, 100, 100, 300)
+        self.setGeometry(100, 100, 300, 400)
 
         self.buttons()
 
     def buttons(self):
         layout = QVBoxLayout()
 
-
-        button_grid = QGridLayout()
-        buttons = [
-            'Normal', 'Math', 'Acc'
-        ]
-
-
-
+        button_mode_layout = QHBoxLayout()
+        normal_button = QPushButton('Normal')
+        math_button = QPushButton('Math')
+        acc_button = QPushButton('Acc')
+        button_mode_layout.addWidget(normal_button)
+        button_mode_layout.addWidget(math_button)
+        button_mode_layout.addWidget(acc_button)
+        layout.addLayout(button_mode_layout)
 
         self.result_label = QLabel('0')
+        self.result_label.setAlignment(Qt.AlignRight)
+        # self.result_label.setAlignment(Qt.AlignBottom)
+        self.result_label.setFont(QFont("Times", 32))
         layout.addWidget(self.result_label)
 
         button_grid = QGridLayout()
+
         buttons = [
             'C', 'AC', '', '',
             '7', '8', '9', '/',
@@ -48,13 +52,11 @@ class My_calculator(QWidget):
 
         self.setLayout(layout)
 
-
 def main():
     app = QApplication([])
     calculator = My_calculator()
     calculator.show()
     app.exec_()
 
-
-main()
-
+if __name__ == "__main__":
+    main()
