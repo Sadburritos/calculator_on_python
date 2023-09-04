36dcb4375ec108bec0a21d737c12170ee6d807dd
Sadburritos <134071785+Sadburritos@users.noreply.github.com>
2023-09-04 21:30:11 +0600

    1

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
-
+import os
 from PyQt5.QtWidgets import QApplication
 from calc_main_window import CalcMainWindow
 from calc_view import My_calculator
 
 
-
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = "venv/Lib/site-packages/PyQt5/Qt5/plugins/platforms"
 
+    app = QApplication(sys.argv)
 
     window = CalcMainWindow('Calculator')
     view = My_calculator()
